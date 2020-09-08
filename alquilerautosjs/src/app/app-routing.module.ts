985ec4c2d7f914a102add8ff5200da03f4e75585
Alex <arevaloalex9@gmail.com>
2020-09-08 15:18:40 -0500

    bootstrap

--- alquilerautosjs/src/app/app-routing.module.ts
+++ alquilerautosjs/src/app/app-routing.module.ts
@@ -1,10 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {AgenciasComponent} from './components/agencias/agencias.component';
+import {CatalogoComponent} from './components/catalogo/catalogo.component';
+import {HomeComponent} from './components/home/home.component';
+import {NosotrosComponent} from './components/nosotros/nosotros.component';
+import {OfertasComponent} from './components/ofertas/ofertas.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'catalogo',
+    component: CatalogoComponent
+  },
+  {
+    path: 'agencias',
+    component: AgenciasComponent
+  },
+  {
+    path: 'ofertas',
+    component: OfertasComponent
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- alquilerautosjs/src/app/app.module.ts
+++ alquilerautosjs/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { HomeComponent } from './components/home/home.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { OfertasComponent } from './components/ofertas/ofertas.component';
+import { AgenciasComponent } from './components/agencias/agencias.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AutocardComponent } from './components/autocard/autocard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent,
+    HomeComponent,
+    CatalogoComponent,
+    NosotrosComponent,
+    OfertasComponent,
+    AgenciasComponent,
+    FooterComponent,
+    AutocardComponent
   ],
   imports: [
     BrowserModule,
